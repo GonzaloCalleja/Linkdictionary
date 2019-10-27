f0a76ced5dea932f29e7d7ad237ae9f37ad39d84
Gonzalo <gonzacalleja@gmail.com>
2019-10-27 10:20:05 +0100

    Work

--- src/LinkDictionary.java
+++ src/LinkDictionary.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import com.company.SimpleSortedInsert;
+
+import java.io.*;
 
 public class LinkDictionary {
+
+    public static void main(String[] args){
+
+        SimpleSortedInsert app = new SimpleSortedInsert();
+        try {
+            app.run();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
